2ee24ef79da92554c1fa878599e37a183b03e676
James Maynard <jameskirkmaynard@gmail.com>
2015-04-11 18:06:41 -0700

    Add interval and prepare for testing.

--- bot.js
+++ bot.js
@@ -10,31 +10,61 @@ var connection = mysql.createConnection({
 	database: 'blah'
 });
 
-getRandomRow = 'SELECT text, used FROM [table] WHERE used=FALSE ORDER BY RAND() LIMIT 1';
+var leoHour = [];
+var leoMinute = [];
 
-function leoSpeaks() {
-
-	connection.query(getRandomRow, function(err, result, fields) {
-		if (err) throw err;
-		else {
-			for (key in result)
-				valueRand = result[key];
-			};
-				console.log(valueRand.text)
-				var quote = valueRand.text
-		connection.query('UPDATE [table] SET used=TRUE WHERE text=' + mysql.escape(quote), valueRand, function(err, result) {
-		});		
-	
-		newTweet.post('statuses/update', { status: quote }, function(error, data, response) {
-			if (error) console.log(error)
-				else
-			console.log(data);
-		});
-	});	
-
+function whenToday() {
+	var numberToday = Math.round(Math.random() * 4);
+		for (i = 0; i < numberToday; i++) {
+			 leoHour[i] = Math.round((Math.random() * 11) + 8);
+			 leoMinute[i] = Math.round(Math.random() * 59);
+				console.log("morning" + leoHour[i]);
+				console.log("morning" + leoMinute[i]);
+		};
 }
 
-setInterval(leoSpeaks, 1000 * 60 * 240);
+// getRandomRow = 'SELECT text, used FROM [table] WHERE used=FALSE ORDER BY RAND() LIMIT 1';
+//
+// function leoSpeaks() {
+//
+// 	connection.query(getRandomRow, function(err, result, fields) {
+// 		if (err) throw err;
+// 		else {
+// 			for (key in result)
+// 				valueRand = result[key];
+// 			};
+// 				console.log(valueRand.text)
+// 				var quote = valueRand.text
+// 		connection.query('UPDATE [table] SET used=TRUE WHERE text=' + mysql.escape(quote), valueRand, function(err, result) {});
+//
+// 		newTweet.post('statuses/update', { status: quote }, function(error, data, response) {
+// 			if (error) console.log(error)
+// 				else
+// 			console.log(data);
+// 		});
+// 	});
+//
+// };
+//
+// setInterval(leoSpeaks, 1000 * 60 * 240);
+//
+// leoSpeaks();
 
-leoSpeaks();
+setInterval(function() {
+		var rightNow = new Date();
+		if (rightNow.getHours() === 1 && rightNow.getMinutes() === 0) {
+			leoHour = [];
+			leoMinute = [];
+			whenToday();
+		} else {
+			for (i = 0; i < leoHour.length; i++) {
+				if (rightNow.getHours() === leoHour[i] && rightNow.getMinutes() === leoMinute[i]) {
+					console.log(i);
+					console.log(leoHour[i]);
+					console.log(leoMinute[i]);
+				};
+			};
+		};
+	}, (1000 * 60));
 
+whenToday();
